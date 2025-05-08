d094eb7c3461dd53763e4bc8df08921d68395ff1
Sahandfer <karimiali0022@yahoo.com>
2025-05-08 17:14:10 +0800

    Updated config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ }) => ({
-  base: "/Sahandfer.github.io/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
